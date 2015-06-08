e2151dba91ab59b84f36a5fea52ab8c3d5c3caff
Andrew Beverley <a.beverley@ctrlo.com>
2015-06-08 06:52:02 +0000

    Add new permissions table schemas

---
+++ lib/Lenio/Schema/Result/LoginPermission.pm
@@ -0,0 +1,117 @@
+use utf8;
+package Lenio::Schema::Result::LoginPermission;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+Lenio::Schema::Result::LoginPermission
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 COMPONENTS LOADED
+
+=over 4
+
+=item * L<DBIx::Class::InflateColumn::DateTime>
+
+=back
+
+=cut
+
+__PACKAGE__->load_components("InflateColumn::DateTime");
+
+=head1 TABLE: C<login_permission>
+
+=cut
+
+__PACKAGE__->table("login_permission");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+
+=head2 login_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 permission_id
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+  "login_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "permission_id",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 login
+
+Type: belongs_to
+
+Related object: L<Lenio::Schema::Result::Login>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "login",
+  "Lenio::Schema::Result::Login",
+  { id => "login_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+=head2 permission
+
+Type: belongs_to
+
+Related object: L<Lenio::Schema::Result::Permission>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "permission",
+  "Lenio::Schema::Result::Permission",
+  { id => "permission_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07025 @ 2015-04-12 22:31:32
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:IhocuVcdLTOrl5LGKuWyJA
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
