4055e9e3761b0cad0c5ba60df9b84d322d614060
Andy Beverley <a.beverley@ctrlo.com>
2017-03-10 20:02:28 +0000

    Add ticket number to invoice

--- lib/Lenio/Schema/Result/Invoice.pm
+++ lib/Lenio/Schema/Result/Invoice.pm
@@ -103,6 +103,8 @@ sub pdf
         y     => 630,
     );
 
+    $page->stringl($font, 10, 400, 620, "Case Number: " . ($org->case_number||'') );
+
     $page->rectangle(72, 500, 450, 40);
     $page->line(222, 540, 222, 500);
     $page->line(372, 540, 372, 500);
@@ -110,7 +112,7 @@ sub pdf
 
     $page->stringl($font, 10, 77, 517, "Invoice Date: ".$self->datetime->strftime($options{dateformat}) );
     $page->stringl($font, 10, 227, 517, "Invoice No: $number");
-    $page->stringl($font, 10, 377, 517, "Case Number: " . ($org->case_number||'') );
+    $page->stringl($font, 10, 377, 517, "Ticket Number: ".$self->ticket->id );
 
     $page->stringl($fontbold, 10, 70, 470, "Professional Fees:");
     my $description = 'Expense reclaim in relation to '.$self->description;
