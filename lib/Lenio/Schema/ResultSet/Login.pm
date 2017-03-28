d9a13d75a96bc30e7ef9e2db1436694ba642c54a
Andy Beverley <a.beverley@ctrlo.com>
2017-03-28 14:47:09 +0100

    Add missing resultset file

---
+++ lib/Lenio/Schema/ResultSet/Login.pm
@@ -0,0 +1,21 @@
+package Lenio::Schema::ResultSet::Login;
+
+use strict;
+use warnings;
+use base qw(DBIx::Class::ResultSet);
+
+sub active_rs
+{   shift->search({
+        deleted => undef,
+    }, {
+        order_by => [
+            'me.surname', 'me.firstname',
+        ],
+    });
+}
+
+sub active
+{   shift->active_rs->all;
+}
+
+1;
